d1a20412fd6b8caf549a368072257662074b3284
Frank Gerold <franketsgerold@gmail.com>
2019-10-15 00:54:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 
 def find_the_cheese (food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food[food.find{|item| if cheese_types.include?}]
+  food[food.find{|item| if cheese_types.include?(item)}]
 end
